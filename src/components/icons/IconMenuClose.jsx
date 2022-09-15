b1d8174b76f0f8a94a16f46d99f3d6deecd30916
bluuweb <contacto@bluuweb.cl>
2022-09-15 16:26:18 -0400

    dia 2

---
+++ src/components/icons/IconMenuClose.jsx
@@ -0,0 +1,18 @@
+const IconMenuClose = (props) => {
+    return (
+        <svg
+            width={16}
+            height={16}
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="m14.364.222 1.414 1.414L9.414 8l6.364 6.364-1.414 1.414L8 9.414l-6.364 6.364-1.414-1.414L6.586 8 .222 1.636 1.636.222 8 6.586 14.364.222z"
+                // fillRule="evenodd"
+                // opacity={0.201}
+            />
+        </svg>
+    );
+};
+
+export default IconMenuClose;
---
+++ src/components/icons/IconMenuOpen.jsx
@@ -0,0 +1,17 @@
+const IconMenuOpen = (props) => {
+    return (
+        <svg
+            width={20}
+            height={14}
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M20 12v2H0v-2h20zm0-6v2H0V6h20zm0-6v2H0V0h20z"
+                fill="#FFF"
+                fillRule="evenodd"
+            />
+        </svg>
+    );
+};
+export default IconMenuOpen;
